72df81d4a8e067f3cc76e45d105ab474e9fcf455
Thomas de Graaff <tdgraaff@sp.nl>
2016-10-24 10:57:10 +0200

    Themes amendments index on amendments overview page.

--- amendments.tpl.php
+++ amendments.tpl.php
@@ -1,30 +1,37 @@
-<h1 id="inhoud">Overzicht alle ingediende amendementen</h1>
+<h2 id="inhoud">Overzicht alle ingediende amendementen</h2>
 <?php if (!empty($meeting['documents'])) : ?>
-
   <ul class="first navigation">
     <?php foreach ($meeting['documents'] as $document) : ?>
       <li class="document">
-        <a href="#document<?php print $document['id']; ?>"><strong><?php print $document['title']; ?></strong></a>
-        <p>Amendement index</p>
-        <ul class="chapters">
-          <?php if (!empty($document['chapters'])): ?>
+        <a href="#document<?php print $document['id']; ?>"><strong>Document: <?php print $document['title']; ?></strong></a>
+        <?php if (!empty($document['chapters'])): ?>
+          <ul class="chapters tab">
+            <li class="chapter"><span>Hoofdstuk:</span></li>
             <?php foreach ($document['chapters'] as $chapter) : ?>
               <li class="chapter">
-                <a href="#chapter<?php print $chapter['nr']; ?>">Hoofdstuk <?php print $chapter['nr']; ?></a>
-								<ul class="amendments">
-									<?php $amendment_ids = $document['amendment_index'][$chapter['nr']]; ?>
-									<?php foreach ($amendment_ids as $amendment_id) : ?>
-										<?php $amendment = $document['amendments'][$amendment_id]; ?>
-										<li>
-											<a href="#amendment<?php print $amendment['id']; ?>"><?php print (!empty($amendment['chapterized_id'])) ? $amendment['chapter'] . '.' . $amendment['chapterized_id'] : $amendment['chapter'] . '.' . $amendment_id; ?></a>
-										</li>
-									<?php endforeach; ?>
-								</ul>
-
+                <a href="#" class="tablinks" id="<?php print 'tab-'.$document['id'].'-'.$chapter['nr']; ?>"><?php print $chapter['nr']; ?></a>
               </li>
             <?php endforeach; ?>
-          <?php endif; ?>
-        </ul>
+          </ul>
+          <?php foreach ($document['chapters'] as $chapter) : ?>
+            <div id="<?php print 'tabcontent-'.$document['id'].'-'.$chapter['nr']; ?>" class="tabcontent">
+              <ul class="amendments">
+                <li class="amendment">
+                  <a href="#chapter<?php print $chapter['nr']; ?>">Hoofdstuk <?php print $chapter['nr']; ?></a>
+                </li>
+              </ul>
+              <ul class="amendments">
+                <?php $amendment_ids = $document['amendment_index'][$chapter['nr']]; ?>
+                <?php foreach ($amendment_ids as $amendment_id) : ?>
+                  <?php $amendment = $document['amendments'][$amendment_id]; ?>
+                  <li class="amendment">
+                    <a href="#amendment<?php print $amendment['id']; ?>"><?php print (!empty($amendment['chapterized_id'])) ? $amendment['chapter'] . '.' . $amendment['chapterized_id'] : $amendment['chapter'] . '.' . $amendment_id; ?></a>
+                  </li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          <?php endforeach; ?>
+        <?php endif; ?>
       </li>
     <?php endforeach; ?>
   </ul>
--- ammo.js
+++ ammo.js
@@ -1,22 +1,36 @@
 (function ($) {
 
-	$(document).ready(function(){
-		$('.text-limit').each(function(){
-			var index = $(this).html().lastIndexOf(' ', 300);
-			if ($(this).html().length > 350) {
-				$(this).html("<span>" + $(this).html().substr(0, index) + "</span><span style='display:none'>" + $(this).html().substr(index) + "</span> <span>... </span><a href='#' class='text-limit-toggle'>&#9658; Meer</a>"); 
-		  }
-		});
+	Drupal.behaviors.ammo = {
+		attach: function (context, settings) {
 
-		$('.text-limit-toggle').bind('click',function(e){
-			e.preventDefault();
-			$(this).prev().prev().toggle();
-			var html = $(this).html();
-			$(this).html( html == "► Meer" ? "◄ Minder" : "► Meer"); 
-			var html = $(this).prev().html();
-			$(this).prev().html( html == "... " ? " " : "... "); 
+				$('.text-limit').each(function(){
+					var index = $(this).html().lastIndexOf(' ', 300);
+					if ($(this).html().length > 350) {
+						$(this).html("<span>" + $(this).html().substr(0, index) + "</span><span style='display:none'>" + $(this).html().substr(index) + "</span> <span>... </span><a href='#' class='text-limit-toggle'>&#9658; Meer</a>"); 
+					}
+				});
 
-		});
-	}); // doc-ready
+				$('.text-limit-toggle').bind('click',function(e){
+					e.preventDefault();
+					$(this).prev().prev().toggle();
+					var html = $(this).html();
+					$(this).html( html == "► Meer" ? "◄ Minder" : "► Meer"); 
+					var html = $(this).prev().html();
+					$(this).prev().html( html == "... " ? " " : "... "); 
+
+				});
+
+				$('.tablinks').bind('click',function(e){
+					e.preventDefault();
+					$('.tabcontent').hide();
+					$('.tablinks.active').removeClass('active');
+					$(this).addClass('active');
+          var tabId = $(this).attr('Id');
+          var contentId = '#tabcontent-' + tabId.substring(4);
+					$(contentId).show();
+				});
+
+		}
+	};
 
 })(jQuery);
