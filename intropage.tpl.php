90693cfdf1869c69ea6cb09d7dcda09c6bfee007
Thomas de Graaff <tdgraaff@sp.nl>
2019-01-14 15:20:54 +0100

    Implements functionality for 'no motions' setting in 0bcbfeeae18dbde6a5a3db23b75fe2a64aeaf7f3.

--- intropage.tpl.php
+++ intropage.tpl.php
@@ -1,11 +1,13 @@
+<?php $enmoties = ($show_motions) ? ' en moties ': ''; ?>
+<?php $ofmoties = ($show_motions) ? ' of moties ': ''; ?>
 <div class="ammo-content">
-  <h1>SP Wijzigingsvoorstellen en Moties</h1>
-  <p><strong>Let op!</strong><br/>Wijzigingsvoorstellen of moties van individuele leden kunnen uitsluiten worden ingediend via het landelijk secretariaat (secretariaat@sp.nl). Deze voorstellen dienen te worden ondersteund door ten minste 50 leden (handtekeningenlijst met naam en lidnummer bijvoegen).</p>
+  <h1>SP Wijzigingsvoorstellen<?php print $enmoties; ?></h1>
+  <p><strong>Let op!</strong><br/>Wijzigingsvoorstellen<?php print $ofmoties; ?> van individuele leden kunnen uitsluiten worden ingediend via het landelijk secretariaat (secretariaat@sp.nl). Deze voorstellen dienen te worden ondersteund door ten minste 50 leden (handtekeningenlijst met naam en lidnummer bijvoegen).</p>
   <p><strong>U kunt in dit scherm:</strong></p>
   <ul>
-    <li>Een overzicht van <?php print l('wijzigingsvoorstellen', 'ammo/amendments'); ?> of <?php print l('moties', 'ammo/motions'); ?> bekijken.</li>
+    <li>Een overzicht van <?php print l('wijzigingsvoorstellen', 'ammo/amendments'); ?><? print ($show_motions) ? ' of ' . l('moties', 'ammo/motions') : ''; ?> bekijken.</li>
     <?php if ($add_access): ?>
-      <li>Nieuwe <?php print l('wijzigingsvoorstellen', 'ammo/amendment'); ?> en <?php print l('moties', 'ammo/motion'); ?> indienen.</li>
+      <li>Nieuwe <?php print l('wijzigingsvoorstellen', 'ammo/amendment'); ?><?php print ($show_motions) ? ' en ' . l('moties', 'ammo/motion') : ''; ?> indienen.</li>
     <?php endif; ?>
     <?php if ($superadmin_access): ?>
       <li>Een nieuwe <?php print l('bijeenkomst toevoegen', 'ammo/meeting'); ?>.</li>
@@ -15,16 +17,16 @@
   <p><strong>In een overzicht kunt u:</strong></p>
   <ul>
     <?php if ($add_access): ?>
-      <li>Wijzigingsvoorstellen en moties van andere afdelingen mede indienen.</li>
+      <li>Wijzigingsvoorstellen<?php print $enmoties; ?> van andere afdelingen mede indienen.</li>
       <?php if (!$admin_access): ?>
-        <li>Uw ingediende wijzigingsvoorstellen en moties aanpassen.</li>
+        <li>Uw ingediende wijzigingsvoorstellen<?php print $enmoties; ?> aanpassen.</li>
       <?php endif; ?>
       <?php if ($admin_access): ?>
-        <li>Ingediende wijzigingsvoorstellen en moties aanpassen.</li>
-        <li>Ingediende wijzigingsvoorstellen en moties voorzien van advies.</li>
-        <li>De status van ingediende wijzigingsvoorstellen en moties aanpassen.</li>
+        <li>Ingediende wijzigingsvoorstellen<?php print  $enmoties; ?> aanpassen.</li>
+        <li>Ingediende wijzigingsvoorstellen<?php print  $enmoties; ?> voorzien van advies.</li>
+        <li>De status van ingediende wijzigingsvoorstellen<?php $enmoties; ?> aanpassen.</li>
       <?php endif; ?>
-      <li>Uw ingediende wijzigingsvoorstellen en moties intrekken.</li>
+      <li>Uw ingediende wijzigingsvoorstellen<?php print $enmoties; ?> intrekken.</li>
     <?php endif; ?>
     <!--<li>Individueel ingediende wijzigingsvoorstellen en moties moties steunen.</li>-->
   </ul>
