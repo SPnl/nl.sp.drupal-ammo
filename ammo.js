ad244c2730ff4da0f208f859999edd68e2d34c66
Thomas de Graaff <thomasdegraaff-office@protonmail.com>
2023-05-12 11:55:59 +0200

    Removes amendment 'more' and 'less' javascript functionality in display.

--- ammo.js
+++ ammo.js
@@ -3,23 +3,6 @@
   Drupal.behaviors.ammo = {
     attach: function (context, settings) {
 
-        $('.text-limit').each(function(){
-          var index = $(this).html().lastIndexOf(' ', 300);
-          if ($(this).html().length > 350) {
-            $(this).html("<span>" + $(this).html().substr(0, index) + "</span><span style='display:none'>" + $(this).html().substr(index) + "</span> <span>... </span><a href='#' class='text-limit-toggle'>&#9658; Meer</a>"); 
-          }
-        });
-
-        $('.text-limit-toggle').bind('click',function(e){
-          e.preventDefault();
-          $(this).prev().prev().toggle();
-          var html = $(this).html();
-          $(this).html( html == "► Meer" ? "◄ Minder" : "► Meer"); 
-          var html = $(this).prev().html();
-          $(this).prev().html( html == "... " ? " " : "... "); 
-
-        });
-
         $('.tablinks').bind('click',function(e){
           e.preventDefault();
           $('.tabcontent').removeClass('active');
